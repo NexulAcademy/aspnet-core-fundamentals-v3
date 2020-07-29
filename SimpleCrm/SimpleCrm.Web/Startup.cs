25d75e53bce9f709cace00aad95bbf703ab514bd
Michael Lang <michael.lang@nexul.com>
2020-07-29 16:44:02 -0500

    Step002: change the hard-coded greeting message

--- SimpleCrm/SimpleCrm.Web/Startup.cs
+++ SimpleCrm/SimpleCrm.Web/Startup.cs
@@ -32,7 +32,7 @@ namespace SimpleCrm.Web
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Bonjour le monde!");
                 });
             });
         }

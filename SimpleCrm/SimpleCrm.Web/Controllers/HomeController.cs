d7a60bbd435d32edb3b1da1d70d1305b1d64c725
Michael Lang <michael.lang@nexul.com>
2020-08-13 15:23:41 -0500

    Step015: add/use new home page view model

--- SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.Web.Models.Home;
 
 namespace SimpleCrm.Web.Controllers
 {
     public class HomeController : Controller
     {
         private ICustomerData _customerData;
+        private readonly IGreeter _greeter;
 
-        public HomeController(ICustomerData customerData)
+        public HomeController(ICustomerData customerData,
+            IGreeter greeter)
         {
             _customerData = customerData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var model = _customerData.GetAll();
+            var model = new HomePageViewModel();
+            model.Customers = _customerData.GetAll();
+            model.CurrentMessage = _greeter.GetGreeting();
             return View(model);
         }
     }
---
+++ SimpleCrm/SimpleCrm.Web/Models/Home/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleCrm.Web.Models.Home
+{
+    public class HomePageViewModel
+    {
+        public string CurrentMessage { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+}

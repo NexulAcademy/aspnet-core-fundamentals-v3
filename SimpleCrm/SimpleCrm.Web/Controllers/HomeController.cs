d3910c3dbd2ba0df11c63ad57518583c271efec1
Michael Lang <michael.lang@nexul.com>
2020-08-12 22:40:25 -0500

    Step014: mvc razor view customers, data interface

--- SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.Web/Controllers/HomeController.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SimpleCrm.Web.Models;
 
 namespace SimpleCrm.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private ICustomerData _customerData;
+
+        public HomeController(ICustomerData customerData)
+        {
+            _customerData = customerData;
+        }
+
         public IActionResult Index()
         {
-            var model = new CustomerModel
-            {
-                Id= 1,
-                FirstName = "Michael",
-                LastName = "Lang",
-                PhoneNumber = "314-555-1234"
-            };
+            var model = _customerData.GetAll();
             return View(model);
         }
     }
---
+++ SimpleCrm/SimpleCrm/ICustomerData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleCrm
+{
+    public interface ICustomerData
+    {
+        IEnumerable<Customer> GetAll();
+    }
+}

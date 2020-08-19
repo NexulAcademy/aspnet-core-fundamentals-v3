5ca8397278b64fa5932bbf661c48282e700c5804
Michael Lang <michael.lang@nexul.com>
2020-08-18 22:38:15 -0500

    Step021: create SqlCustomerData and a DbContext

---
+++ SimpleCrm/SimpleCrm.SqlDbServices/SimpleCrmDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SimpleCrm.SqlDbServices
+{
+    public class SimpleCrmDbContext : DbContext
+    {
+        public SimpleCrmDbContext(DbContextOptions<SimpleCrmDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ SimpleCrm/SimpleCrm.SqlDbServices/SqlCustomerData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCrm.SqlDbServices
+{
+    public class SqlCustomerData : ICustomerData
+    {
+        private SimpleCrmDbContext _context;
+        public SqlCustomerData(SimpleCrmDbContext context)
+        {
+            _context = context;
+        }
+
+        public Customer Get(int id)
+        {
+            return _context.Customers.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public void Save(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+    }
+}

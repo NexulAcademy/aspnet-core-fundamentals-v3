3aa507960c4d787553c29aab982248d821606233
Michael Lang <michael.lang@nexul.com>
2020-09-12 14:03:18 -0500

    Step034: account registration started

---
+++ SimpleCrm/SimpleCrm.Web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.Web.Models.Account;
+
+namespace SimpleCrm.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            return NoContent(); //TODO: validate model, register the user
+        }
+    }
+}
---
+++ SimpleCrm/SimpleCrm.Web/Models/Account/RegisterUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCrm.Web.Models.Account
+{
+    public class RegisterUserViewModel
+    {
+        [Required, MaxLength(256), DisplayName("Email Address"), DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+        [Required, MaxLength(256), DisplayName("Name")]
+        public string DisplayName { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}

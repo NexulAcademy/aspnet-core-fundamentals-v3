31fb4c0f7769718adabe4c47000a9d23e3b14674
Michael Lang <michael.lang@nexul.com>
2020-10-28 11:31:53 -0500

    61: added form validation for customer add dialog

--- SimpleCrm/simple-crm-cli/src/app/customer/customer-create-dialog/customer-create-dialog.component.ts
+++ SimpleCrm/simple-crm-cli/src/app/customer/customer-create-dialog/customer-create-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Customer } from '../customer.model';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'crm-customer-create-dialog',
@@ -17,10 +17,10 @@ export class CustomerCreateDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: Customer | null
   ) {
     this.detailForm = this.fb.group({
-      firstName: [''],
-      lastName: [''],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
       phoneNumber: [''],
-      emailAddress: [''],
+      emailAddress: ['', [Validators.required, Validators.email]],
       preferredContactMethod: ['email']
     });
     if (this.data) {
@@ -32,13 +32,14 @@ export class CustomerCreateDialogComponent implements OnInit {
   }
 
   cancel(): void {
-    // TODO: close this dialog
     this.dialogRef.close();
   }
 
   save(): void {
-    // TODO: get form data and return to parent component
-    const customer = {};
+    if (!this.detailForm.valid) {
+      return;
+    }
+    const customer = { ...this.data, ...this.detailForm.value };
     this.dialogRef.close(customer);
   }
 

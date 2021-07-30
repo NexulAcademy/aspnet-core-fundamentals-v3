b943a12077357cf4b5f43ea2d060497490928df8
Michael Lang <michael.lang@nexul.com>
2021-07-30 18:35:42 -0500

    112: marketing page content

--- SimpleCrm/SimpleCrm.WebApi/Controllers/HomeController.cs
+++ SimpleCrm/SimpleCrm.WebApi/Controllers/HomeController.cs
@@ -1,33 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using SimpleCrm.WebApi.Models;
 
 namespace SimpleCrm.WebApi.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [Route("home")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        [Route("privacy")]
         public IActionResult Privacy()
         {
             return View();
         }
 
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        [Route("corporate")]
+        public IActionResult CorporateClients()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Any)]
+        [Route("pricing")]
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+
+        [Route("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

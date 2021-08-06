f1ac478e79cab909018ee9eeffda3c385c7e065e
Michael Lang <michael.lang@nexul.com>
2021-08-05 20:13:10 -0500

    118: get user data with JWT for authenticated user

--- SimpleCrm/SimpleCrm.WebApi/ApiControllers/AuthController.cs
+++ SimpleCrm/SimpleCrm.WebApi/ApiControllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SimpleCrm.WebApi.Auth;
 using SimpleCrm.WebApi.Models.Auth;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SimpleCrm.WebApi.ApiControllers
@@ -8,10 +10,15 @@ namespace SimpleCrm.WebApi.ApiControllers
     public class AuthController : Controller
     {
         private readonly UserManager<CrmUser> _userManager;
+        private readonly IJwtFactory _jwtFactory;
 
-        public AuthController(UserManager<CrmUser> userManager)
+        public AuthController(
+            UserManager<CrmUser> userManager,
+            IJwtFactory jwtFactory
+        )
         {
             _userManager = userManager;
+            _jwtFactory = jwtFactory;
         }
 
         [HttpPost("login")]
@@ -52,6 +59,35 @@ namespace SimpleCrm.WebApi.ApiControllers
 
             // Credentials are invalid, or account doesn't exist
             return await Task.FromResult<CrmUser>(null);
+        }
+
+        private async Task<UserSummaryViewModel> GetUserData(CrmUser user)
+        {
+            if (user == null)
+                return null;
+
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Count == 0)
+            {
+                roles.Add("prospect");
+            }
+
+            // generate the jwt for the local user...
+            var jwt = await _jwtFactory.GenerateEncodedToken(user.UserName,
+                _jwtFactory.GenerateClaimsIdentity(user.UserName, user.Id.ToString()));
+            var userModel = new UserSummaryViewModel
+            {   //JWT could inject all these properties instead of creating a model,
+                //but a model is a little easier to access from client code without
+                //decoding the token. When this user model starts to contain arrays
+                //of complex data, including it all in the JWT value can get complicated.
+                Id = user.Id,
+                Name = user.DisplayName,
+                EmailAddress = user.Email,
+                JwtToken = jwt,
+                Roles = roles.ToArray(), //each role could be a separate claim in the JWT
+                AccountId = 0 //TODO: load this from registration data
+            };
+            return userModel;
         }
     }
 }
---
+++ SimpleCrm/SimpleCrm.WebApi/Models/Auth/UserSummaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleCrm.WebApi.Models.Auth
+{
+    public class UserSummaryViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string JwtToken { get; set; }
+        public string[] Roles { get; set; }
+        //TODO: add other app specific capabilities for the user
+        // one option...
+        public int AccountId { get; set; }
+    }
+}
